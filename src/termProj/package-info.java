13c309ee2f4d98d87c28a455b5792d653ff0cb94
kialessa <n.speck24@gmail.com>
2017-02-16 19:25:27 -0500

    Package Info Update

--- src/termProj/package-info.java
+++ src/termProj/package-info.java
@@ -1,7 +1,7 @@
 /**
  * A collection of luck-testing and fortune-telling games. <p>
  * Made as a class project for CIS 351, section 02.
- * @author Natasha Speck
+ * @author Bridget Bieniek, Madison Brooks, Natasha Speck
  * @version 1.3, 2017/02/16
  */
 package termProj;
